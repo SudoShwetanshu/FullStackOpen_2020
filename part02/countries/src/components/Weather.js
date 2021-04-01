6b7b32ecf62bbd71c9a987bb5eae5718a37f8fbf
Shwetanshu_Bot <67130226+SudoShwetanshu@users.noreply.github.com>
2021-04-01 08:30:39 +0530

    Update Weather.js

--- part02/countries/src/components/Weather.js
+++ part02/countries/src/components/Weather.js
@@ -5,13 +5,13 @@ const Weather = ({weatherData}) => {
 
     return( <>
         <div>
-          <h2>{`Weather in ${weatherData?.location.name}`}</h2>
+          <h2>{`Weather in ${weatherData?.location?.name}`}</h2>
         </div>
-        <div> {`temperature: ${(weatherData?.current.temperature)}` }
+        <div> {`temperature: ${(weatherData?.current?.temperature)}` }
         <br />
-        <img src={weatherData?.current.weather_icons} /> 
+        <img src={weatherData?.current?.weather_icons} /> 
         <br />
-        {`wind: ${weatherData?.current.wind_speed} mph direction ${weatherData?.current.wind_dir}`}
+        {`wind: ${weatherData?.current?.wind_speed} mph direction ${weatherData?.current?.wind_dir}`}
             
         </div> </>
     )
